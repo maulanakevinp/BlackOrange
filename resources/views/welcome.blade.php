8fe35e15d90cc751ffe249f3cb8616373e785cf1
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2020-09-10 12:55:05 +0700

    first-commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Black Orange CCTV</title>
+    <meta name="description" content="Website resmi Black Orange CCTV" />
+    <meta name="author" content="pesan-web.com" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,900">
+    <link rel="stylesheet"
+        href="https://garudev.com/wp-content/plugins/under-construction-page/themes/css/bootstrap.min.css?v=3.80"
+        type="text/css">
+    <link rel="stylesheet"
+        href="https://garudev.com/wp-content/plugins/under-construction-page/themes/css/common.css?v=3.80"
+        type="text/css">
+    <link rel="stylesheet"
+        href="https://garudev.com/wp-content/plugins/under-construction-page/themes/mad_designer/style.css?v=3.80"
+        type="text/css">
+    <link rel="stylesheet"
+        href="https://garudev.com/wp-content/plugins/under-construction-page/themes/css/font-awesome.min.css?v=3.80"
+        type="text/css">
+    <link rel="icon" sizes="128x128"
+        href="https://garudev.com/wp-content/plugins/under-construction-page/themes/images/favicon.png" />
+</head>
+
+<body>
+    <div id="hero-image">
+        <img src="https://garudev.com/wp-content/plugins/under-construction-page/themes/mad_designer/mad-designer.png"
+            alt="Mad Designer at work" title="Mad Designer at work">
+    </div>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-xs-12 col-md-12 col-lg-12">
+                <h1>Maaf, kami sedang mengerjakan beberapa pekerjaan di situs ini</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-offset-2 col-lg-8">
+                <p class="content">Terima kasih sudah bersabar. Kami sedang mengerjakan beberapa pekerjaan di situs
+                    ini
+                    dan akan segera kembali.</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
