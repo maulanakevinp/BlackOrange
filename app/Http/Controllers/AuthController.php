f4f0aae4741cbfd05c9bf9ed503cd334770a5c87
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2020-09-14 22:32:16 +0700

    update ganti password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Rules\LoginRule;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -20,7 +22,21 @@ class AuthController extends Controller
 
     public function update(Request $request)
     {
-        # code...
+        $user = User::find(auth()->user()->id);
+
+        $request->validate([
+            'password_lama'         => ['required','string','min:8'],
+            'password_baru'         => ['required','string','min:8'],
+            'ulangi_password_baru'  => ['required','string','same:password_baru'],
+        ]);
+
+        if (Hash::check($request->password_lama, $user->password)) {
+            $user->password = Hash::make($request->password_baru);
+            $user->save();
+            return redirect()->back()->with('success','Password berhasil diperbarui');
+        } else {
+            return redirect()->back()->with('error','Password lama yang anda masukkan salah');
+        }
     }
 
     public function masuk(Request $request)
--- resources/views/ganti-password.blade.php
+++ resources/views/ganti-password.blade.php
@@ -10,6 +10,27 @@
                 <div class="col-lg-6">
                     <div class="pt-80 hero-cap hero-cap2">
                         <h2 class="text-center">GANTI PASSWORD</h2>
+                        <form action="{{ route('ganti-password') }}" method="post">
+                            @csrf @method('patch')
+                            <div class="form-group">
+                                <label for="">Password Lama</label>
+                                <input type="password" name="password_lama" id="password_lama" class="form-control @error('password_lama') is-invalid @enderror" placeholder="Masukkan Password Lama ..." value="{{ old('password_lama') }}">
+                                @error('password_lama') <span class="invalid-feedback">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password Baru</label>
+                                <input type="password" name="password_baru" id="password_baru" class="form-control @error('password_baru') is-invalid @enderror" placeholder="Masukkan Password Baru ..." value="{{ old('password_baru') }}">
+                                @error('password_baru') <span class="invalid-feedback">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="">Ulangi Password Baru</label>
+                                <input type="password" name="ulangi_password_baru" id="ulangi_password_baru" class="form-control @error('ulangi_password_baru') is-invalid @enderror" placeholder="Ulangi Password Baru ..." value="{{ old('ulangi_password_baru') }}">
+                                @error('ulangi_password_baru') <span class="invalid-feedback">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn header-btn">Simpan</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
